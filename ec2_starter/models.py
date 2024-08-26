ee307ba275d4db32724ac74a661c05f419389480
Leigh West <leighwest@hotmail.com>
2024-08-26 19:19:33 +1000

    Updated EC2 model, request all EC2 instances from DB on starting page load, initial layout for EC2 instance component

--- ec2_starter/models.py
+++ ec2_starter/models.py
@@ -5,5 +5,6 @@ class Ec2(models.Model):
     instance_id = models.CharField(max_length=20, unique=True)
     region = models.CharField(max_length=20)
     name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500, blank=True)
     state = models.CharField(max_length=20)
     last_start_time = models.DateTimeField(null=True, blank=True)
--- ec2_starter/views.py
+++ ec2_starter/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
+from .models import Ec2
+
+
 def starting_page(request):
-    return render(request, "ec2_starter/index.html")
+    instances = Ec2.objects.all()
+    return render(request, "ec2_starter/index.html", {
+        "instances": instances
+    })
